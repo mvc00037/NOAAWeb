3e1e56e7db8230c09117426a288061d854ddccb3
mvc00037 <48292538+mvc00037@users.noreply.github.com>
2023-10-14 12:14:17 +0200

    Modify return

--- render/views.py
+++ render/views.py
@@ -27,4 +27,4 @@ def prediction(request,idSat ):
         sat_data.append(satResponse)
     context = {'sat_data': sat_data}
 
-    return render(request, 'noaaWebApp/prueba.html', context)
+    return render(request, 'render/predictionTable.html.html', context)

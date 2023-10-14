05bb9cea9a02ac479f0c5d83d317a331437ba094
mvc00037 <48292538+mvc00037@users.noreply.github.com>
2023-10-14 12:07:58 +0200

    Add urlpatterns

--- render/urls.py
+++ render/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from render.views import index
+from render.views import index,prediction
 
 urlpatterns = [
     path('', index, name='index'),
+    path('prediction/<str:idSat>', prediction, name='prediction'),
 ]
--- render/views.py
+++ render/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.shortcuts import render
+import requests, datetime
 
 def index(request):
-    return render(request, 'render/extends.html', {})
+    return render(request, 'render/index.html', {})
+
+def prediction(request,idSat ):
+    sat_data = []
+    url = 'https://api.n2yo.com/rest/v1/satellite/radiopasses/{}/36.76/-3.44/500/10/40/&apiKey=8LTCAG-YNFHC9-K96TEM-4WW6'
+
+    r = requests.get(url.format(idSat)).json()
+    iterator = r['info']['passescount']
+
+    for i in range(iterator):
+        satResponse = {
+            'satname': r['info']['satname'],
+            'satId': r['info']['satid'],
+            "transactionscount": r['info']['transactionscount'],
+            'startAz': r['passes'][i]['startAz'],
+            'endAz': r['passes'][i]['endAz'],
+            'maxEl' :  r['passes'][i]['maxEl'],
+            "startUTC": datetime.datetime.fromtimestamp(r['passes'][i]['startUTC'],tz= pytz.timezone('Europe/Madrid')).strftime('%Y-%m-%d %H:%M:%S'),
+            "endUTC": r['passes'][i]['endUTC'],
+            "maxEl": r['passes'][i]['maxEl'],
+        }
+        sat_data.append(satResponse)
+    context = {'sat_data': sat_data, "sat": sat}
+
+    return render(request, 'noaaWebApp/prueba.html', context)

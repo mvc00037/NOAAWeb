cfdaf040bb508241b345258ab56f2678983428bf
mvc00037 <48292538+mvc00037@users.noreply.github.com>
2023-10-24 22:52:44 +0200

    M

--- noaaWeb/settings.py
+++ noaaWeb/settings.py
@@ -156,8 +156,8 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
+MEDIA_ROOT = BASE_DIR / "media"
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
 
 
 # Default primary key field type

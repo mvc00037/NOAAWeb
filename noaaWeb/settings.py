133c5fea28ac3165546da05657f45f750cb4224c
mvc00037 <48292538+mvc00037@users.noreply.github.com>
2023-10-24 22:32:01 +0200

    Modify settings

--- noaaWeb/settings.py
+++ noaaWeb/settings.py
@@ -136,12 +136,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -149,22 +143,22 @@ STATIC_URL = '/static/'
 # Here, they well be accessible at your-domain.onrender.com/static/...
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    # The absolute path to the directory where collectstatic will collect static files for deployment.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    )
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-    MEDIA_URL = '/media/'
-    #MEDIA_ROOT = BASE_DIR / "media"
-    MEDIA_ROOT =  os.path.join(BASE_DIR, 'media'),
+MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

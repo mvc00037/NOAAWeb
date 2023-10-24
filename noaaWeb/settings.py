fd25e447515e2aee68411b9178861247dc524385
mvc00037 <48292538+mvc00037@users.noreply.github.com>
2023-10-24 23:15:36 +0200

    Create folder media

--- noaaWeb/settings.py
+++ noaaWeb/settings.py
@@ -157,7 +157,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media'), a
 
 
 # Default primary key field type
